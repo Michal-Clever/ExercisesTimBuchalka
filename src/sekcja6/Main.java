e62313a91d2eeb9d4defa667bcc96d0a28aa2cd2
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-01 14:19:13 +0200

    Sekcja 6

---
+++ src/sekcja6/Main.java
@@ -0,0 +1,11 @@
+package sekcja6;
+
+public class Main {
+    public static void main(String[] args) {
+        //SEKCJA 6
+        System.out.println("Exercises 11 :");
+
+
+        System.out.println("**********");
+    }
+}
