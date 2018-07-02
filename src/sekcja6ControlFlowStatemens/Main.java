492fdbd322b7e8cd9570662a44d6ec37aec26839
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-02 20:56:52 +0200

    EvenDigitsSumm

--- src/sekcja6ControlFlowStatemens/Main.java
+++ src/sekcja6ControlFlowStatemens/Main.java
@@ -157,7 +157,9 @@ public class Main {
         //Write a method named getEvenDigitSum with one parameter of type int called number.  The
         //method should return the sum of the even digits within the number.  If the number is
         //negative, the method should return -1 to indicate an invalid value
-
+        System.out.println(EvenDigitsSum.getEvenDigitSum(123456789));
+        System.out.println(EvenDigitsSum.getEvenDigitSum(252));
+        System.out.println(EvenDigitsSum.getEvenDigitSum(-22));
         System.out.println("**********");
     }
 }
