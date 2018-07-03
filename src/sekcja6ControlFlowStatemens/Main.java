8326771e646ec3b429934c9a77495d3581ad355e
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-03 23:05:09 +0200

    GreatestCommonDivisor

--- src/sekcja6ControlFlowStatemens/Main.java
+++ src/sekcja6ControlFlowStatemens/Main.java
@@ -261,5 +261,31 @@ public class Main {
         System.out.println(" ");FactorPrinter.printFactors(10);
         System.out.println(" ");FactorPrinter.printFactors(-1);
         System.out.println("**********");
+
+        System.out.println("Exercises 21 Perfect Number :");
+        //What is a perfect number? A perfect number is a positive integer which is equal to the
+        //sum of its proper positive divisors.  Proper positive divisors are positive integers that fully
+        //divide the perfect number without leaving a remainder and exclude the perfect number
+        //itself. For example, take the number 6:  Its proper divisors are 1, 2, and 3 (since 6 is the
+        //value of the perfect number, it is excluded), and the sum of its proper divisors is 1 + 2 + 3 =
+        //6. 
+        //Therefore, 6 is a perfect number (as well as the rst
+        //perfect number).
+        //Write a method named isPerfectNumber with one parameter of type int named number. 
+        //If number is < 1, the method should return false.
+        //The method must calculate if the number is perfect. If the number is perfect, the method
+        //should return true; otherwise, it should return false.
+        //EXAMPLE INPUT/OUTPUT:
+        //isPerfectNumber(6); should return true since its proper divisors are 1, 2, 3 and the
+        //sum is 1 + 2 + 3 = 6
+        //isPerfectNumber(28); should return true since its proper divisors are 1, 2, 4, 7, 14
+        //and the sum is 1 + 2 + 4 + 7 + 14 = 28
+        //isPerfectNumber(5); should return false since its only proper divisor is 1 and the
+        //sum is 1 not 5
+        System.out.println(PerfectNumber.isPerfectNumber(6));
+        System.out.println(PerfectNumber.isPerfectNumber(28));
+        System.out.println(PerfectNumber.isPerfectNumber(5));
+        System.out.println(PerfectNumber.isPerfectNumber(-1));
+        System.out.println("**********");
     }
 }
