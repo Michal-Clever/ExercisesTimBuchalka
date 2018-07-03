2e79a011c3052e7007a34ded18054afa07cd6f5d
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-03 22:15:59 +0200

    GreatestCommonDivisor

--- src/sekcja6ControlFlowStatemens/Main.java
+++ src/sekcja6ControlFlowStatemens/Main.java
@@ -223,25 +223,28 @@ public class Main {
         //The method should return the greatest common divisor of the two numbers (int).
         //The greatest common divisor is the largest positive integer that can fully divide each of
         //the integers (i.e. without leaving a remainder).
-        //                For example 12 and 30:
-        //                12 can be divided by 1, 2, 3, 4, 6, 12
-        //                30 can be divided by 1, 2, 3, 5, 6, 10, 15, 30
-        //                The greatest common divisor is 6 since both 12 and 30 can be divided by 6, and there is no
-        //                resulting remainder
-        //                EXAMPLE INPUT/OUTPUT:
-        //                getGreatestCommonDivisor(25, 15); should return 5 since both can be divided by 5
-        //                without a remainder
-        //                getGreatestCommonDivisor(12, 30); should return 6 since both can be divided by 6
-        //                without a remainder
-        //                getGreatestCommonDivisor(9, 18); should return -1 since the rst
-        //                parameter is <
-        //                10
-        //                getGreatestCommonDivisor(81, 153); should return 9 since both can be divided by
-        //                9 without a remainder
+        //For example 12 and 30:
+        //12 can be divided by 1, 2, 3, 4, 6, 12
+        //30 can be divided by 1, 2, 3, 5, 6, 10, 15, 30
+        //The greatest common divisor is 6 since both 12 and 30 can be divided by 6, and there is no
+        //resulting remainder
+        //EXAMPLE INPUT/OUTPUT:
+        //getGreatestCommonDivisor(25, 15); should return 5 since both can be divided by 5
+        //without a remainder
+        //getGreatestCommonDivisor(12, 30); should return 6 since both can be divided by 6
+        //without a remainder
+        //getGreatestCommonDivisor(9, 18); should return -1 since the rst
+        //parameter is <
+        //10
+        //getGreatestCommonDivisor(81, 153); should return 9 since both can be divided by
+        //9 without a remainder
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(25, 15));
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(12, 30));
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(9, 18));
         System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(81, 153));
+        System.out.println("**********");
+
+        System.out.println("Exercises 20  :");
 
         System.out.println("**********");
     }
