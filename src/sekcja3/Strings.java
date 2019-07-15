9001f7cf940aa616d6cea19804d2a25807bc57cf
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-15 22:45:24 +0200

    Tim

--- src/sekcja3/Strings.java
+++ src/sekcja3/Strings.java
@@ -1,4 +1,17 @@
 package sekcja3;
 
 public class Strings {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println(myString);
+
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+    }
+
 }
