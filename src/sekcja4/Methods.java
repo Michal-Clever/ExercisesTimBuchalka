7a3e95ccfa30db528be7b76ae90f64fea3a0ff8f
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-18 00:54:19 +0200

    Tim

--- src/sekcja4/Methods.java
+++ src/sekcja4/Methods.java
@@ -2,22 +2,66 @@ package sekcja4;
 
 public class Methods {
     public static void main(String[] args) {
+
+
+
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        caltulateScore(true,800,levelCompleted,bonus);
+        int highScore = caltulateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was : " + highScore + " point");
 
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        highScore = caltulateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was : " + highScore + " point");
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Adam",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Bob",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Gilbert",highScorePosition);
 
     }
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position "
+                + highScorePosition + " o the high score table");
+    }
 
-    public static void caltulateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
+    public static int caltulateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was : " + finalScore + " point");
+            finalScore += 1000;
+            return finalScore;
         }
+        return -1;
     }
+
+
+
+
 }
