e6341ce5724da642cb58cbc59149e580aeabb9b0
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-17 18:49:19 +0200

    Tim

--- src/sekcja4/IfKeywordAndCodeBlocks.java
+++ src/sekcja4/IfKeywordAndCodeBlocks.java
@@ -3,5 +3,32 @@ package sekcja4;
 public class IfKeywordAndCodeBlocks {
     public static void main(String[] args) {
 
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if(score<5000){
+//            System.out.println("Your score was lass than 5000");
+//        }else{
+//            System.out.println("Got here");
+//        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was : " + finalScore + " point");
+        }
+
+
+        gameOver = true;
+        score = 10_000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was : " + finalScore + " point");
+        }
+
     }
 }
