b47820a283dbb0ad336edfba7d27a017d1e7e80b
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-18 01:06:22 +0200

    Tim

--- src/sekcja4/Methods.java
+++ src/sekcja4/Methods.java
@@ -4,7 +4,6 @@ public class Methods {
     public static void main(String[] args) {
 
 
-
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
@@ -21,34 +20,47 @@ public class Methods {
         System.out.println("Your final score was : " + highScore + " point");
 
         int highScorePosition = calculateHighScorePosition(1500);
-        displayHighScorePosition("Adam",highScorePosition);
+        displayHighScorePosition("Adam", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(900);
-        displayHighScorePosition("Bob",highScorePosition);
+        displayHighScorePosition("Bob", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(400);
-        displayHighScorePosition("Percy",highScorePosition);
+        displayHighScorePosition("Percy", highScorePosition);
 
         highScorePosition = calculateHighScorePosition(50);
-        displayHighScorePosition("Gilbert",highScorePosition);
+        displayHighScorePosition("Gilbert", highScorePosition);
 
     }
+
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " managed to get into position "
                 + highScorePosition + " o the high score table");
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//        return 4;
+
+        int position = 4;
+
         if (playerScore >= 1000) {
-            return 1;
+            position = 1;
         } else if (playerScore >= 500) {
-            return 2;
+            position = 2;
         } else if (playerScore >= 100) {
-            return 3;
-        } else {
-            return 4;
+            position = 3;
         }
+        return position;
+
     }
+
 
     public static int caltulateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
@@ -60,8 +72,6 @@ public class Methods {
         }
         return -1;
     }
-
-
 
 
 }
