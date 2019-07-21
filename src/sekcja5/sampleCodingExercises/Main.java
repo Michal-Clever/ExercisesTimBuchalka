a0a742c836ee00444d9ef1775db37cefca063b3e
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-21 21:24:48 +0200

    Tim

---
+++ src/sekcja5/sampleCodingExercises/Main.java
@@ -0,0 +1,11 @@
+package sekcja5.sampleCodingExercises;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(10);
+
+    }
+}
