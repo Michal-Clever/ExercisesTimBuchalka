9acb29f82ac1b93a134399c64ff1d68be3e0cf69
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-22 22:02:53 +0200

    Tim

---
+++ src/sekcja5/While.java
@@ -0,0 +1,11 @@
+package sekcja5;
+
+public class While {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count != 5) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
+}
