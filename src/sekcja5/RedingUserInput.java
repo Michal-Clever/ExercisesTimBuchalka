59afd59b8e277e4312f8ef63e08f1c7e2c77d5e3
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-25 23:08:53 +0200

    Tim

---
+++ src/sekcja5/RedingUserInput.java
@@ -0,0 +1,26 @@
+package sekcja5;
+
+import java.util.Scanner;
+
+public class RedingUserInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your  year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter you name: ");
+        String name = scanner.nextLine();
+        int age = 2019 - yearOfBirth;
+
+        System.out.println("Your name is " + name + " , and you are " + age + " year old.");
+
+        scanner.close();
+
+
+    }
+
+}
