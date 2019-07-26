5e95f63872d1b6c471bf57ef15d7731bf48a9f6b
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-26 23:41:44 +0200

    Tim

--- src/sekcja5/ReadingUserInputChallenge.java
+++ src/sekcja5/ReadingUserInputChallenge.java
@@ -8,11 +8,26 @@ public class ReadingUserInputChallenge {
         Scanner scanner = new Scanner(System.in);
         int counter = 0;
         int sum = 0;
-        while (true) {
+        //       while (true) {
+        while (counter < 10) {
             int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+//                if (counter == 10) {
+//                    break;
+//                }
+            } else {
+                System.out.println("Invalid number");
+            }
+            scanner.nextLine();
         }
-
-
+        System.out.println("sum = " + sum);
         scanner.close();
     }
 }
