332c46ca1376741ca87a79c1186043685fc8433d
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-26 23:23:08 +0200

    Tim

---
+++ src/sekcja5/ReadingUserInputChallenge.java
@@ -0,0 +1,18 @@
+package sekcja5;
+
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+        int sum = 0;
+        while (true) {
+            int order = counter + 1;
+        }
+
+
+        scanner.close();
+    }
+}
