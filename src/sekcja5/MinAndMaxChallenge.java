e5855ea29910c79a64514d0611194dbf287aa1e4
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-27 00:40:01 +0200

    Tim

--- src/sekcja5/MinAndMaxChallenge.java
+++ src/sekcja5/MinAndMaxChallenge.java
@@ -7,20 +7,38 @@ public class MinAndMaxChallenge {
 
         Scanner scanner = new Scanner(System.in);
 
-        int min = 0;
-        int max = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+//        boolean first = true;
+
         while (true) {
             System.out.println("Enter number: ");
             boolean isAnInt = scanner.hasNextInt();
-            if(isAnInt){
+            if (isAnInt) {
+
+                int number = scanner.nextInt();
+
+//                if (first) {
+//                    first = false;
+//                    min = number;
+//                    max = number;
+//                }
+
+                if (number > max) {
+                    max = number;
+                }
+
+                if (number < min) {
+                    min = number;
+                }
 
 
-            }else{
-                System.out.println("Invalid number");
+            } else {
+                break;
             }
-
+            scanner.nextLine(); // handle input
         }
-
+        System.out.println("min = " + min + " max = " + max);
         scanner.close();
     }
 }
