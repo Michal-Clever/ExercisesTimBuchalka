9e918fa5425e48021d54c3fa9749ca2a971546a5
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-07-28 13:26:23 +0200

    Exercise

--- src/sekcja6/Classes.java
+++ src/sekcja6/Classes.java
@@ -3,5 +3,9 @@ package sekcja6;
 public class Classes {
     public static void main(String[] args) {
 
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carrera");
     }
 }
