a8251f1ae8aa9b5a2e8651ef6982ef1f1d187089
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-04 22:29:42 +0200

    Exercise

--- src/sekcja6/animal/Dog.java
+++ src/sekcja6/animal/Dog.java
@@ -9,7 +9,6 @@ public class Dog extends Animal {
     private int teeth;
     private String coat;
 
-
     public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
         super(name, 1, 1, size, weight);
         this.eyes = eyes;
