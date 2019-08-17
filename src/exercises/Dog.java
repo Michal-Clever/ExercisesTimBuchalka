f17e3cb2015aa2cfa6668dade790af52b8236fd0
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-17 09:27:54 +0200

    Exercise

---
+++ src/exercises/Dog.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class Dog {
+    private static String name;
+
+    public Dog(String name) {
+        Dog.name = name;
+    }
+
+    public void printName() {
+        System.out.println("name = " + name);
+    }
+
+}
---
+++ src/exercises/Main.java
@@ -0,0 +1,12 @@
+package exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog rex = new Dog("rex");
+        Dog fluffy = new Dog("fluffy");
+        rex.printName();
+        fluffy.printName();
+    }
+
+}
