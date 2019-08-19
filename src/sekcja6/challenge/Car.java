a1747a44bfe0be0dfa96cf12be2edcd168d00e03
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-20 00:06:39 +0200

    Exercise

--- src/sekcja6/challenge/Car.java
+++ src/sekcja6/challenge/Car.java
@@ -1,4 +1,23 @@
 package sekcja6.challenge;
 
-public class Car {
+public class Car extends Vehicle {
+
+    private int wheels;
+    private int dors;
+    private int gears;
+    private boolean isManual;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int dors, int gears, boolean isManual) {
+        super(name, size);
+        this.wheels = wheels;
+        this.dors = dors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.currentGear = 1;
+    }
+
+
+
 }
