d72536a32d5b31765a34e77e52c5bdd8b7c27791
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-22 22:52:41 +0200

    Exercise

--- src/sekcja7/playerEncapsulation/Main.java
+++ src/sekcja7/playerEncapsulation/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
         EnhancedPlayer player = new EnhancedPlayer("Songo", 450, "sword");
         System.out.println("Inital health is " + player.getHealth());
+        player.loseHealth(20);
+        System.out.println("Inital health is " + player.getHealth());
+        player.loseHealth(180);
+        System.out.println("Inital health is " + player.getHealth());
 
     }
 
