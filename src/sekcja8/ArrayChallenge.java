140d68531149a67bfa05a5c8707390db8d8ebadc
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-25 15:21:18 +0200

    Exercise

--- src/sekcja8/ArrayChallenge.java
+++ src/sekcja8/ArrayChallenge.java
@@ -1,5 +1,6 @@
 package sekcja8;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayChallenge {
@@ -8,12 +9,13 @@ public class ArrayChallenge {
     public static void main(String[] args) {
 
         int[] myIntegers = getIntegers(5);
-
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
 
     }
 
     public static int[] getIntegers(int capacatiy) {
-        System.out.println(" Enter : " + capacatiy + " integer values: \r");
+        System.out.println(" Enter " + capacatiy + " integer values: \r");
         int[] array = new int[capacatiy];
         for (int i = 0; i < array.length; i++) {
             array[i] = scanner.nextInt();
@@ -29,15 +31,21 @@ public class ArrayChallenge {
     }
 
     public static int[] sortIntegers(int[] array) {
-        int[] sortedArray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            sortedArray[i] = array[i];
-        }
+//        int[] sortedArray = new int[array.length];
+//        for (int i = 0; i < array.length; i++) {
+//            sortedArray[i] = array[i];
+//            }
+// Skócony zapis:
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+
         boolean flag = true;
         int temp;
-        while (flag) {
+        while (flag)
+
+        {
             flag = false;
-            for (int i = 0; i < sortedArray.length; i++) {
+            for (int i = 0; i < sortedArray.length - 1; i++) {
                 if (sortedArray[i] < sortedArray[i + 1]) {
                     temp = sortedArray[i];
                     sortedArray[i] = sortedArray[i + 1];
