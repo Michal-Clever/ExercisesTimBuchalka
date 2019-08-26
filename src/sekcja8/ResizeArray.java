c4e1e60e4c019aeeb576b4bd9834e6054bc9fb60
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-26 20:18:30 +0200

    Exercise

--- src/sekcja8/ResizeArray.java
+++ src/sekcja8/ResizeArray.java
@@ -8,10 +8,16 @@ public class ResizeArray {
     private static int[] baseDate = new int[10];
 
     public static void main(String[] args) {
-
+        System.out.println("Enter 10 integers: ");
+        getInput();
+        printArray(baseDate);
+        resizeArray();
+        System.out.println("Enter 12 integers: ");
+        getInput();
+        printArray(baseDate);
     }
 
-    private void getInput() {
+    private static void getInput() {
         for (int i = 0; i < baseDate.length; i++) {
             baseDate[i] = scanner.nextInt();
         }
@@ -19,7 +25,7 @@ public class ResizeArray {
 
     private static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i] + " ");
+            System.out.print(array[i] + " ");
             System.out.println();
         }
     }
