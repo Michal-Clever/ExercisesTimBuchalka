c0903066d74cf4000aa127deb9fd211db2749d10
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-27 21:48:43 +0200

    Exercise

--- src/sekcja8/Main.java
+++ src/sekcja8/Main.java
@@ -8,10 +8,64 @@ public class Main {
     private static GroceryList groceryList = new GroceryList();
 
     public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstruction();
+        while (!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
-
+            switch (choice) {
+                case 0:
+                    printInstruction();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    serchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
     }
 
+    public static void printInstruction() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item int rhe list.");
+        System.out.println("\t 6 - To quit the application.p");
+    }
+
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
+        groceryList.addGroceryIteam(scanner.nextLine());
+    }
+
+    public static void modifyItem() {
+        System.out.print("Enter item number: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter replacement item: ");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryIteam(itemNo - 1,newItem);
+    }
 
 
 }
