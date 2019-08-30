a7226d918a967b82f4cedd63333ae7c198e86f77
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-08-30 06:36:29 +0200

    Exercise

--- src/sekcja8/autoboxingAndUnboxing/Branch.java
+++ src/sekcja8/autoboxingAndUnboxing/Branch.java
@@ -16,9 +16,20 @@ public class Branch {
     }
 
     public boolean newCustomer(String customerName, double initialAmount) {
-        if (findCustomer(customerName == null)){
-            this.customers.add(newCustomer(customerName,initialAmount));
+        if (findCustomer(customerName == null)) {
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer exisingCustomer = findCustomer(customerName);
+
+        if (exisingCustomer != null) {
+            exisingCustomer.addTransactions(amount);
         }
     }
+
 
 }
