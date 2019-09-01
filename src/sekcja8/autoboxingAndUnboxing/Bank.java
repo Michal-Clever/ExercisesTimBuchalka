27c42da1e21c5f37def99b028320b88fdfbe22f3
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-09-01 14:10:18 +0200

    Exercise

--- src/sekcja8/autoboxingAndUnboxing/Bank.java
+++ src/sekcja8/autoboxingAndUnboxing/Bank.java
@@ -21,7 +21,7 @@ public class Bank {
     }
 
 
-    public boolean addCustomer(String branchName, String customerName, int initialAmount) {
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
         Branch branch = findBranch(branchName);
         if (branch != null) {
             return branch.newCustomer(customerName, initialAmount);
@@ -38,16 +38,37 @@ public class Bank {
     }
 
     private Branch findBranch(String branchName) {
-        for(int i=0; i<this.branches.size(); i++) {
+        for (int i = 0; i < this.branches.size(); i++) {
             Branch checkedBranch = this.branches.get(i);
-            if(checkedBranch.getName().equals(branchName)) {
+            if (checkedBranch.getName().equals(branchName)) {
                 return checkedBranch;
             }
         }
         return null;
     }
 
+    public boolean listCustomers(String branchName, boolean showTransactions) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            System.out.println("Customers details for branch " + branch.getName());
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for (int i = 0; i < branchCustomers.size(); i++) {
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i + 1) + "]");
+                if (showTransactions) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + j + "] Amount " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+
+        } else {
+            return false;
+        }
 
 
-
+    }
 }
--- src/sekcja8/autoboxingAndUnboxing/Main.java
+++ src/sekcja8/autoboxingAndUnboxing/Main.java
@@ -1,4 +1,24 @@
 package sekcja8.autoboxingAndUnboxing;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Bank bank = new Bank ("National Australia Bank");
+
+        bank.addBranch("Adelaide");
+
+        bank.addCustomer("Adelaide", "Tim", 12.50);
+        bank.addCustomer("Adelaide" , " Mike ", 143.58);
+        bank.addCustomer("Adelide","Percy",220.14);
+
+        bank.addBranch("Sydney");
+        bank.addCustomer("Sydney","Bob",150.77);
+
+        bank.addCustomerTransaction("Adelaide","Tim",556.78);
+        bank.addCustomerTransaction("Adelaide","Mike",6.78);
+        bank.addCustomerTransaction("Adelaide","Percy",100.00);
+
+        bank.listCustomers("Adelaide",true);
+
+    }
 }
