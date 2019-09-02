74c1bf2fa8046d7f717f6c48f39595dab1a6be99
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-09-02 19:13:06 +0200

    Exercise

--- src/sekcja8/DemoLinkedList.java
+++ src/sekcja8/DemoLinkedList.java
@@ -2,6 +2,7 @@ package sekcja8;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class DemoLinkedList {
     public static void main(String[] args) {
@@ -14,17 +15,48 @@ public class DemoLinkedList {
         placeToVisit.add("Canberra");
         placeToVisit.add("Adelaide");
         placeToVisit.add("Darwin");
-
-
         printList(placeToVisit);
+
+
+        placeToVisit.add(1, "Alice Springs");
+        printList(placeToVisit);
+
+
+        placeToVisit.remove(4);
+        printList(placeToVisit);
+
 
     }
 
     private static void printList(LinkedList<String> linkedList) {
         Iterator<String> i = linkedList.iterator();
         while (i.hasNext()) {
-            System.out.println("Now visiting" + i.next());
+            System.out.println("Now visiting " + i.next());
         }
         System.out.println("=======================");
     }
+
+    private static boolean addInOrder (LinkedList<String>linkedList,String newCity){
+        ListIterator<String> stringListIterator=linkedList.listIterator();
+        while (stringListIterator.hasNext()){
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if(comparison == 0){
+                //equal, do not add
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            }else if (comparison>0){
+                // new City should appear befor this one
+                // Brisbane -> Adelaide
+                stringListIterator.previous(); // returns to the record
+                stringListIterator.add(newCity);
+                return true;
+
+            }
+        }
+    }
+
+
+
+
+
 }
