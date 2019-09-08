4742d280c62e641c1f92c1af87ea3ff49afc2d1e
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-09-08 21:39:18 +0200

    Exercise

--- src/sekcja9/abstractClasses/challeng/Main.java
+++ src/sekcja9/abstractClasses/challeng/Main.java
@@ -8,7 +8,6 @@ public class Main { public static void main(String[] args) {
     String stringData = "5 7 3 9 8 2 1 0 4 6";
 //        String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
 
-
     String[] data = stringData.split(" ");
     for (String s : data) {
         tree.addItem(new Node(s));
