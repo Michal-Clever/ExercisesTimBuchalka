e169f1b7a5dd087a5ee4b6629b8077b73e1e6fad
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-09-17 19:07:07 +0200

    Exercise

--- src/sekcja12/Main.java
+++ src/sekcja12/Main.java
@@ -1,4 +1,11 @@
 package sekcja12;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now());
+    }
 }
