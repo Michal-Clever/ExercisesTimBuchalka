a134045b38617e85d6d5b242a6975ee8b52941a6
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-09-19 08:03:32 +0200

    Exercise

--- src/sekcja12/adventure/Main.java
+++ src/sekcja12/adventure/Main.java
@@ -16,6 +16,7 @@ public class Main {
         //
         // Single letter commands (N, W, S, E, Q) should still be available.
 
+
 	    Scanner scanner = new Scanner(System.in);
 
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
