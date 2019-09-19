93be19470d938c6abdbf2d4cff393686109c05d6
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-09-19 07:54:39 +0200

    Exercise

--- src/sekcja12/adventure/Location.java
+++ src/sekcja12/adventure/Location.java
@@ -3,20 +3,24 @@ package sekcja12.adventure;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Created by dev on 8/12/2015.
+ */
 public class Location {
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        this.exits = new HashMap<String, Integer>(exits);
+        this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public int getLocationID() {
         return locationID;
